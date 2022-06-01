3efe7ebc510d016233dd55ad6706956491c558d4
dina <dina.rotenberg483@gmail.com>
2022-06-02 02:11:10 +0300

    Initial commit

---
+++ src/test/java/gb/dina/AppTest.java
@@ -0,0 +1,20 @@
+package gb.dina;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
